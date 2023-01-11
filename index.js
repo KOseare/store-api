609c293ca5b5b9a8968d2460972b07ee97149143
Dario Funes <kdfunes@outlook.com>
2023-01-11 19:41:41 -0300

    Add cors library

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const routerApi = require('./routes');
 
 const {logErrors, errorHandler, boomErrorHandler} = require('./middlewares/error.handler');
@@ -7,6 +8,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors()); // TO DO: Implement whitelist
 
 app.get('/', (req, res) => {
   res.send('OK!');
