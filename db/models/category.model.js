70eb9c1b5b1f070a32d55227d63adf74fa16711a
Dario Funes <kdfunes@outlook.com>
2023-01-24 17:06:34 -0300

    Create product and categery and add relation 'one to many' between them

---
+++ db/models/category.model.js
@@ -0,0 +1,50 @@
+const {Model, DataTypes} = require('sequelize');
+
+const CATEGORY_TABLE = 'categories';
+
+const CategorySchema = {
+  id: {
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    type: DataTypes.INTEGER
+  },
+  name: {
+    allowNull: false,
+    type: DataTypes.STRING,
+    unique: true
+  },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  createdAt: {
+    allowNull: false,
+    type: DataTypes.DATE,
+    field: 'created_at',
+    defaultValue: DataTypes.NOW
+  }
+};
+
+class Category extends Model {
+
+  static associate (models) {
+    this.hasMany(models.Product, {
+      as: 'products',
+      foreignKey: 'categoryId'
+    });
+  }
+
+  static config(sequelize) {
+    return {
+      sequelize,
+      tableName: CATEGORY_TABLE,
+      modelName: 'Category',
+      timestamps: false
+    };
+  }
+
+}
+
+module.exports = {Category, CategorySchema, CATEGORY_TABLE};
+
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const productsRouter = require('./products.router');
 const usersRouter = require('./user.router');
 const customersRouter = require('./customer.router');
+const categoryRouter = require('./category.router');
 
 function routerApi (app) {
   const router = express.Router();
@@ -10,6 +11,7 @@ function routerApi (app) {
   router.use('/products', productsRouter);
   router.use('/users', usersRouter);
   router.use('/customers', customersRouter);
+  router.use('/categories', categoryRouter);
 }
 
 module.exports = routerApi;
